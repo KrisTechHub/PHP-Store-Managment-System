e712e1541cef603c0a3eea1aaf0036f6c3f9d952
KrisTechHub <kmj.algario@gmail.com>
2023-03-10 12:29:05 +0800

    first commit and establish connection

---
+++ storeclass.php
@@ -0,0 +1,29 @@
+<?php
+
+Class MyStore {
+
+    //private can only be accessed inside ccurly braces or inside the class.
+    //public function can access all data inside the class
+    //protected can access var and function inside the sub functions(public), but cannot access outside
+    //protected is used in php packages deployed in different servers, also uses $this
+
+    private $server = "mysql:host=localhost;dbname=mystore";
+    private $user = "root"; 
+    private $password = "Tiggy.143";
+    private $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC); //:: is resolution oeprator, first way of PHP to access (line .(dot) in js)
+    protected $con;
+
+    public function openConnection() { 
+        try { //try catch method
+            $this->con = new PDO($this->server, $this->user, $this->pass, $this->options);
+            return $this->con;
+        } catch(PDOException $e) {
+            echo "There is some problem with the connection :" . $e->getMessage();
+        }
+    }
+
+    public function closeconnection(){ //to optimize program, need to close connection.
+        $this->con = null;
+    }
+
+}
