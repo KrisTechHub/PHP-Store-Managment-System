86f5712ddeb155e2c42a281675684e77f6d089c1
KrisTechHub <kmj.algario@gmail.com>
2023-03-10 15:34:30 +0800

    new login

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nitz' Store | MyStore</title>
+</head>
+<body>
+    <div class="container">
+        <div class="form-container">
+            <form action="" method="post">
+                <div class="form-input">
+                    <label>Username</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="form-input">
+                    <label>Password</label>
+                    <input type="text" name="password" id="password">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
+
